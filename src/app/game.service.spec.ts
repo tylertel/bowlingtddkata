d709899d0bc08132cfd7f726c2544d7e38034e0e
tyler tel <ttel@qualfon.com>
2019-05-03 09:12:25 -0400

    starting progress on writing tests/building service.

--- src/app/game.service.spec.ts
+++ src/app/game.service.spec.ts
@@ -1,7 +1,37 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { GameService } from './game.service';
+import { GameService } from "./game.service";
 
-describe('GameService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe("GameService", () => {
+  const service:GameService = new GameService();
+  let pinsup:number;
+  service.currentPinsUpCount.subscribe(p => pinsup = p);
+  beforeEach(() => TestBed.configureTestingModule({
+    
+  }));
+  
+  it("Pins Left reduces after roll", () => {
+    service.newGame();
+    service.roll(1);
+    service.currentPinsUpCount.subscribe(p => pinsup = p);
+    expect(pinsup).toBe(9);
+  });
+
+  it("New Game starts with 10 pins up",() =>{
+    service.newGame();
+    expect(pinsup).toBe(10);
+  });
+
+  it("Pins left is never less than 1",() =>{
+    service.roll(10);
+    expect(pinsup).toBeGreaterThanOrEqual(1);
+  });
+
+  it("A strike increases the score ten plus previous 2 rolls.",() =>{
+    service.roll(5);
+    service.roll(3);
+    service.roll(10);
+
+    expect(pinsup).toBeGreaterThanOrEqual(1);
+  });
 });
--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,22 +1,32 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class GameService {
-
   public score: BehaviorSubject<number> = new BehaviorSubject(0);
   public currentPinsUpCount: BehaviorSubject<number> = new BehaviorSubject(10);
-  public board: BehaviorSubject<any[]> = new BehaviorSubject([]);
+  public rolls: BehaviorSubject<number[]> = new BehaviorSubject([]);
 
-  constructor() { }
+  constructor() {}
 
-  public roll(int){
+  public roll(pinCount) {
+    let currentPinCount: number;
+    this.currentPinsUpCount.value - pinCount >= 1
+      ? (currentPinCount = this.currentPinsUpCount.value - pinCount)
+      : (currentPinCount = 10);
 
+    this.rolls.next(this.rolls.getValue().concat([pinCount]));
+
+    this.currentPinsUpCount.next(currentPinCount);
   }
 
-  public newGame(){
-    
+  public newGame() {
+    this.currentPinsUpCount.next(10);
+  }
+
+  public calculateScore(){
+    this.score.next(1);
   }
 }
